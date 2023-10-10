6372b9272d14d07b82dcb43bdcaad323cc259b5d
SOWMIYA RAMAMURTHY <144764180+sowmikutty3@users.noreply.github.com>
2023-10-09 23:15:27 -0600

    api.py

---
+++ task3_urlshortner/app/templates/api.py
@@ -0,0 +1,16 @@
+from app.utils import short_url_generator
+from app.utils import url_checker
+from flask import request
+
+
+def apiv10(long_url):
+    # This is our Version-1.0 API function. It will take the long_url as
+    # parameter then return two different dictionary based on url_checker
+    # function output.
+    checker = url_checker(long_url)
+    if checker is True:
+        short_url = short_url_generator(long_url)
+        return {'state' : 'ok', 'long_url' : long_url, 'short_url' : short_url, 'message' : ''}
+    else:
+        error = "The URL seems to be dead at this moment."
+        return {'state' : 'error', 'long_url' : long_url, 'short_url' : '', 'message' : error}
